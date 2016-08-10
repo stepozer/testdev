bcb8b8423ea6a9a221c6eab507e14e088a20b93d
stepozer <stepozer@github.com>
2016-08-10 17:00:27 +0300

    run!

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -34,6 +34,17 @@ class SiteController extends Controller
         if (Yii::$app->user->isGuest) {
             // $this->redirect('user/login');
         }
+        $user = new \app\models\User();
+        $user->username = 'u1';
+        $user->email = 'u1';
+        $user->password = 'u1';
+        $user->created_at = date('Y-m-d');
+        $user->updated_at = date('Y-m-d');
+        $user->save();
+
+        echo '<pre>';
+        print_r(\app\models\User::find()->all());
+        echo '</pre>';
 
         return $this->render('index');
     }
